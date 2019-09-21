f59b888afc56aa80669c060db8f4428ac9a9934e
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-09-21 16:44:33 -0400

    view renamed

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/carta', function () {
+    return view('letter');
 });
