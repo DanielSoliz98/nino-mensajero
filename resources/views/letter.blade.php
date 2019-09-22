74db1bc0da0e708ddc5a47468ba3654470e884a9
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-09-21 20:12:21 -0400

    icons changed position

--- resources/views/letter.blade.php
+++ resources/views/letter.blade.php
@@ -22,7 +22,7 @@
 <body>
     <section class="container-fluid slider d-flex justify-content-center">
         <div class="container">
-            <h1 class="display-4 text-center">
+            <h1 class="display-4 text-center mt-2">
                 <img src="letter.svg" width="30" height="30" class="d-inline-block"alt="">
                 Escribir mi Carta
                 <img src="letter.svg" width="30" height="30" class="d-inline-block"alt="">
@@ -31,8 +31,8 @@
                 <textarea class="form-control text form-rounded border border-primary" id="exampleFormControlTextarea1" rows="14"
                 placeholder="Cuentanos tus experiencias..."></textarea>
                 <div class="d-flex flex-row justify-content-center mt-3">
-                    <button class="btn btn-primary  btn-lg mr-5" type="button"><i class="mr-2 far fa-images"></i>Anadir imagen</button>
-                    <button class="btn btn-primary btn-lg ml-5" type="submit"><i class="mr-2 fas fa-envelope-open-text"></i>Enviar mi carta</button>
+                    <button class="btn btn-primary  btn-lg mr-5" type="button">Anadir imagen <i class="ml-2 far fa-images"></i></button>
+                    <button class="btn btn-primary btn-lg ml-5" type="submit">Enviar mi carta <i class="ml-2 fas fa-envelope-open-text"></i></button>
                 </div>
             </form>
         </div>
