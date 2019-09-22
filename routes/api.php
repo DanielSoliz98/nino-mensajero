0c3f40348178f24f065f3595db0cfea68782ce3a
CristopherSs <alanss2907@gmail.com>
2019-09-21 23:51:37 -0500

    ruta para controlador insertada en el archivo

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Controllers routes
+// Carta
+Route::resource('carta','CartaController');
