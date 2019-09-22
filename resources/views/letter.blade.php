4d91ab4541c69648af62f4de592dc8371fe7324a
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-09-21 20:10:37 -0400

    help button added

--- resources/views/letter.blade.php
+++ resources/views/letter.blade.php
@@ -19,6 +19,9 @@
                 <img src="letter.svg" width="30" height="30" class="d-inline-block"alt="">
             </h1>
             <form>
+                <div class="div d-flex justify-content-end">
+                    <button class="btn btn-primary btn-lg" type="button">Ayuda <i class="far fa-question-circle"></i></button>
+                </div>
                 <textarea class="form-control text form-rounded border border-primary" rows="14"
                 placeholder="Cuentanos tus experiencias..."></textarea>
                 <div class="d-flex flex-row justify-content-center mt-3">
