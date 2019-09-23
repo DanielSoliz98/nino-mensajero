9f0eabf03b5239d53dd64ff164e53db24724a19c
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-09-23 10:22:18 -0400

    Comments added to web.php

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/**
+ * Route for Home(/), returning view from HomeController: create method.
+ */
 Route::get('/','HomeController@create')->name('home');
 
+/**
+ * Route for Write a Letter(/writeLetter), returning  view from LetterController: create method.
+ */
 Route::get('/writeLetter', 'LetterController@create')->name('writeLetter');
 
+/**
+ * Route for Write a Letter(/writeLetter), store the content of the letter on database.
+ * LetterController method store();
+ */
 Route::post('/writeLetter', 'LetterController@store')->name('letter.post');
