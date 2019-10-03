545d45e2cd203d7dfbb7bbe654b05480a1757415
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2019-10-03 12:55:42 -0400

    views refactored

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,8 @@
 @extends('template')
 @section('section')
-<style>
-.backgroud{
-    background: url("background.jpg");
-    height: 92vh;
-    background-size: cover;
-    background-position: center;
-}
-.content{
-    height: 90vh;
-}
-</style>
 <div class="container-fluid backgroud">
-    <div class=" content d-flex align-items-center justify-content-center">
-    <a href="{{route('writeLetter')}}" class="btn btn-primary btn-lg">Escribir mi Carta</a>
+    <div class=" content-home d-flex align-items-center justify-content-center">
+        <a href="{{route('writeLetter')}}" class="btn2"><i class="far fa-envelope"></i> Escribir mi carta</a>
     </div>
 </div>
 @endsection
