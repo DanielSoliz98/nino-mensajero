fb281e3a9ca4a0371327dd0951b83dc56de7329e
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2019-10-13 19:10:10 -0400

    div images refactored

--- resources/views/personal/letter.blade.php
+++ resources/views/personal/letter.blade.php
@@ -5,16 +5,19 @@
         <div class="card-body">
             <p class="card-text">{{$letter->content}}</p>
         </div>
-        @if(count($letter->images) > 0)
-        <div class="card-footer container col-8">
-            <h6 class="card-subtitle mb-2 text-center">Imágenes</h6>
-            @foreach ($letter->images as $image)
-                <img src="/storage/{{$image->filename}}" class="card-img-top mt-2 img-responsive img-thumbnail rounded" 
-                alt="...">
-            @endforeach
+        <div class="container">
+            @if(count($letter->images) > 0)
+                <h6 class="card-subtitle mb-2 text-center">Imágenes</h6>
+                <div class="row">
+                        @foreach ($letter->images as $image)
+                        <div class="col-lg-4 col-md-4 col-xs-6 mt-2">
+                            <img src="/storage/{{$image->filename}}" class="card-img-top img-fluid img-thumbnail rounded" 
+                            alt="...">
+                        </div>
+                        @endforeach
+                </div>
+            @endif
         </div>
-        @endif
-            
         <div class="card-footer text-muted text-center mt-2">
                 {{$letter->created_at->diffForHumans()}} - {{$letter->created_at->format('l j \\d\\e F Y h:i:s A')}}
         </div>
