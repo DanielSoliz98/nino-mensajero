d4b539244d6535f671d805a4fd4f10bca5c7a267
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-10-17 11:15:10 -0400

    perfil deleted from menu

--- resources/views/personal/menu-personal.blade.php
+++ resources/views/personal/menu-personal.blade.php
@@ -1,6 +1,5 @@
 @section('title-menu', 'Menu-Personal')
 @section('menu-list')
-    <a href="" class="list-group-item list-group-item-action bg-light">Mi perfil</a>
     <a href="{{route('letters')}}" class="list-group-item list-group-item-action bg-light">Cartas de Niños</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Informacion Generada</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Notificaciones</a> 
