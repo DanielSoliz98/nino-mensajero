67e6f53866c2a02d6382295c231ed0b51feb1f84
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2019-10-19 20:53:21 -0400

    menu of admin and personal renamed to menu-list

--- resources/views/users/admin/personal-information.blade.php
+++ resources/views/users/admin/personal-information.blade.php
@@ -1,7 +1,7 @@
 @extends('users.menu')
 @section('title', 'Informacion de Personal')
 @section('page-title', 'Personal de Niño Mensajero')
-@include('users.admin.menu')
+@include('users.admin.menu-list')
 @section('user-content')
     <h1>Ejemplo de ventana de info personal</h1>
 @endsection
