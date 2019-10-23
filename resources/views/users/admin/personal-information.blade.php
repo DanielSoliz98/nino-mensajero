8149282a7a34cd9a7d1360aa80e1314100e1b7bd
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-10-23 17:15:43 -0400

    menu of admin deleted

--- resources/views/users/admin/personal-information.blade.php
+++ resources/views/users/admin/personal-information.blade.php
@@ -1,36 +1,34 @@
 @extends('users.menu')
 @section('title', 'Informacion de Personal')
 @section('page-title', 'Informacion de Personal')
-@include('users.admin.menu-list')
 @section('user-content')
-
-<div class="container mt-2">
-    <br>
-    <form enctype="multipart/form-data" method="POST">
-        <div class="d-flex mt-2 mb-4 table-responsive">
-            <table class="table table-striped table-md tablebody" border="1px">
-                <tr>
-                    <th>NOMBRE</th>
-                    <th>CORREO</th>
-                    <th>TELÉFONO</th>
-                    <th>C.I.</th>
-                    <th>USUARIO</th>
-                    <th>PROFESIÓN</th>
-                </tr>
-                @foreach($personals as $pers)
-                <tr>  
-                    <td><a href="{{route('persProfile', $pers)}}">{{$pers->full_name}}</a></td>
-                    <td>{{$pers->email}}</td>
-                    <td>{{$pers->phone}}</td>
-                    <td>{{$pers->id}}</td>
-                    <td>{{$pers->user_name}}</td>
-                    <td>{{$pers->rol_id}}</td> 
-                </tr>
-                @endforeach
-            </table>
-        </div>
-    </form>
-</div>
+    <div class="container mt-2">
+        <br>
+        <form enctype="multipart/form-data" method="POST">
+            <div class="d-flex mt-2 mb-4 table-responsive">
+                <table class="table table-striped table-md tablebody" border="1px">
+                    <tr>
+                        <th>NOMBRE</th>
+                        <th>CORREO</th>
+                        <th>TELÉFONO</th>
+                        <th>C.I.</th>
+                        <th>USUARIO</th>
+                        <th>PROFESIÓN</th>
+                    </tr>
+                    @foreach($personals as $pers)
+                    <tr>  
+                        <td><a href="{{route('persProfile', $pers)}}">{{$pers->full_name}}</a></td>
+                        <td>{{$pers->email}}</td>
+                        <td>{{$pers->phone}}</td>
+                        <td>{{$pers->id}}</td>
+                        <td>{{$pers->user_name}}</td>
+                        <td>{{$pers->rol_id}}</td> 
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </form>
+    </div>
 @endsection
 
 
--- resources/views/users/admin/profile.blade.php
+++ resources/views/users/admin/profile.blade.php
@@ -1,45 +1,43 @@
 @extends('users.menu')
 @section('title', 'Perfil Profesional')
 @section('page-title', 'Perfil Profesional')
-@include('users.admin.menu-list')
 @section('user-content')
-
-<section class="container mt-2">
-    <br>      
-    <form enctype="multipart/form-data" method="POST">
-        <div class="d-flex mt-2 mb-4 table-responsive">
-            <table class="table table-striped table-md tablebody" border="1px">  
-                @foreach ($queryPersProfile as $profile)
-                    <tr>  
-                        <td>Nombre:</td> 
-                        <td>{{$personals->full_name}}</td>
-                    </tr>
-                    <tr>
-                        <td>C.I.</td>
-                        <td>{{$personals->id}}</td>
-                    </tr>
-                    <tr>
-                        <td>Profesión:</td> 
-                        <td>{{$profile->profession}}</td>
-                    </tr>
-                    <tr>
-                        <td>Grado de formación:</td> 
-                        <td>{{$profile->degree}}</td>
-                    </tr>
-                    <tr>
-                        <td>Especialidades:</td>
-                        <td>{{$profile->specialties}}</td>
-                    </tr> 
-                    <tr>
-                        <td>Tiempo de experiencia:</td>        
-                        <td>{{$profile->experience}}</td>   
-                    </tr>
-                @endforeach
-            </table>
-        </div>
-        <div class="d-flex align-items-center justify-content-center ">
-            <a class="btn btn-light border border-dark" href="{{route('home')}}">Editar perfil</a>
-        </div>
-    </form>
-</section>
+    <section class="container mt-2">
+        <br>      
+        <form enctype="multipart/form-data" method="POST">
+            <div class="d-flex mt-2 mb-4 table-responsive">
+                <table class="table table-striped table-md tablebody" border="1px">  
+                    @foreach ($queryPersProfile as $profile)
+                        <tr>  
+                            <td>Nombre:</td> 
+                            <td>{{$personals->full_name}}</td>
+                        </tr>
+                        <tr>
+                            <td>C.I.</td>
+                            <td>{{$personals->id}}</td>
+                        </tr>
+                        <tr>
+                            <td>Profesión:</td> 
+                            <td>{{$profile->profession}}</td>
+                        </tr>
+                        <tr>
+                            <td>Grado de formación:</td> 
+                            <td>{{$profile->degree}}</td>
+                        </tr>
+                        <tr>
+                            <td>Especialidades:</td>
+                            <td>{{$profile->specialties}}</td>
+                        </tr> 
+                        <tr>
+                            <td>Tiempo de experiencia:</td>        
+                            <td>{{$profile->experience}}</td>   
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+            <div class="d-flex align-items-center justify-content-center ">
+                <a class="btn btn-light border border-dark" href="{{route('home')}}">Editar perfil</a>
+            </div>
+        </form>
+    </section>
 @endsection
