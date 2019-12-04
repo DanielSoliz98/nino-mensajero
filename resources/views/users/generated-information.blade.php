444f361832247c0ce061043cec2a8ed41df545bd
Alison Orellana <ali15.orellan@gmail.com>
2019-12-04 12:57:58 -0400

    titles

--- resources/views/users/generated-information.blade.php
+++ resources/views/users/generated-information.blade.php
@@ -1,14 +1,14 @@
 @extends('users.menu')
-@section('title', 'Información generada')
-@section('page-title', 'Información generada')
+@section('title', 'Informaciones generadas')
+@section('page-title', 'Informaciones generadas')
 @section('user-content')
 <section class="container"> 
     <form enctype="multipart/form-data" method="POST">
         <div class="d-flex table-responsive">
             <table class="table table-striped table-md tablebody mt-3 ml-2 mr-2 mb-3" border="1px">
                 <tr>
-                    <th>ID CARTAS</th>
-                    <th>CONTENIDO DE CARTAS</th>
+                    <th>ID CARTA</th>
+                    <th>CONTENIDO DE CARTA</th>
                     <th>CANTIDAD DE <br>RESPUESTAS</th>
                 </tr>
                 @foreach($informations as $info)
