07ed90c45ebec4fa7b0bc5146534954755b19467
Mauricio Cadima Guevara <mauricio.cadima.guevara08@gmail.com>
2019-12-08 10:34:59 -0400

    notifications increase for different types of letters

--- app/Notifications/ImportantLetterNotification.php
+++ app/Notifications/ImportantLetterNotification.php
@@ -43,7 +43,7 @@ class ImportantLetterNotification extends Notification
     {
         return [
             //
-            'repliedTime' =>Carbon::now()
+            'repliedTime'=>Carbon::now()
         ];
     }
 
