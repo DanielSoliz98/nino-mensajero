5be39615fe6424738878408de0e0655fa7f38dae
Mauricio Cadima Guevara <mauricio.cadima.guevara08@gmail.com>
2019-12-09 06:52:25 -0400

    fix message

--- resources/views/users/dangerous_letter_notification.blade.php
+++ resources/views/users/dangerous_letter_notification.blade.php
@@ -1,3 +1,3 @@
 <a class="dropdown-item" href={{ route('user.letter.read', $notification->data['letter']['id']) }}>
-    Tienes una carta de tipo Peligro
+    Recibiste una carta de tipo Peligro
 </a>
