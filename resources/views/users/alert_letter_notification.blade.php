fd8033bae1830f211ee267f587615fa98d5a95f2
CristopherSs <alanss2907@gmail.com>
2019-12-09 23:52:39 -0500

    message upgraded for dropdown of notifications

--- resources/views/users/alert_letter_notification.blade.php
+++ resources/views/users/alert_letter_notification.blade.php
@@ -1,3 +1,3 @@
 <a class="dropdown-item" href={{ route('user.letter.read', $notification->data['letter']['id']) }}>
-    Recibiste una carta de tipo Alerta
+{{substr($notification->data['letter']['content'],0,14)  }}  | URGENTE
 </a>
