5e56de77deed8d4f9ef969b916af86fd8bf74902
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-09 23:18:04 -0400

    message fixed

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -25,7 +25,7 @@ class InformationController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return back()->with('error', 'La informacion generada no puede estar vacia.');
+            return back()->with('error', 'La nformación generada no puede estar vacía.');
         }
 
         $information = GeneratedInformation::create([
