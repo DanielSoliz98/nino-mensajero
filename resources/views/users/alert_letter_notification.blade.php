9a02a720d83738580408e2125619879a974892ba
Mauricio Cadima Guevara <mauricio.cadima.guevara08@gmail.com>
2019-12-10 01:00:24 -0400

    messages were changed

--- resources/views/users/alert_letter_notification.blade.php
+++ resources/views/users/alert_letter_notification.blade.php
@@ -1,3 +1,3 @@
 <a class="dropdown-item" href={{ route('user.letter.read', $notification->data['letter']['id']) }}>
-{{substr($notification->data['letter']['content'],0,14)  }}  | URGENTE
+{{substr($notification->data['letter']['content'],0,14)  }}  | ALERTA
 </a>
