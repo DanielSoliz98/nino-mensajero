8c08a3ae6c944d042b232e558eac067f7baf421c
Mauricio Cadima Guevara <mauricio.cadima.guevara08@gmail.com>
2019-12-09 23:00:21 -0400

    fix errors

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
 function markNotificationAsRead(notificationCount) {
-    if (notificationCount !== 0) {
-        $.get('/markAsRead');
+    // if (notificationCount !== 0) {
+    //     $.get('/markAsRead');
         
-    }
+    // }
+
+    var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", '/markAsRead', false ); // false for synchronous request
+        xmlHttp.send( null );
+        xmlHttp.responseText;
+
 }
+
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
