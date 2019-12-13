0ac2d771d0ef349eb09432c14839766b93658aaa
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-13 11:01:20 -0400

    dependencies added

--- database/seeds/UsersRolesAndPermissionsTablesSeeder.php
+++ database/seeds/UsersRolesAndPermissionsTablesSeeder.php
@@ -18,26 +18,11 @@ class UsersRolesAndPermissionsTablesSeeder extends Seeder
 
         $personal = Role::create(['name' => 'personal']);
         $admin = Role::create(['name' => 'admin']);
-
-        Permission::create(['name' => 'read_letters']);
-        Permission::create(['name' => 'view_profiles']);
-        Permission::create(['name' => 'add_personal']);
-        Permission::create(['name' => 'update_profile']);
-
-        $personal->givePermissionTo(['read_letters', 'update_profile']);
-        $admin->givePermissionTo(['read_letters', 'view_profiles', 'add_personal']);
-
-        $user = User::create([
-            'full_name' => 'Personal',
-            'email' => 'personal@gmail.com',
-            'password' => bcrypt('personal123'),
-        ]);
-        $user->assignRole('personal');
         
         $admin = User::create([
             'full_name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin123'),
+            'email' => 'admin2019@gmail.com',
+            'password' => bcrypt('@dm1n2019'),
         ]);
         $admin->assignRole('admin');
     }
