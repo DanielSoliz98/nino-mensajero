418e109c008aefe58b985a887923469d159af502
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-28 15:52:45 -0400

    columns of bulletins modified

--- resources/views/users/admin/bulletins.blade.php
+++ resources/views/users/admin/bulletins.blade.php
@@ -8,34 +8,47 @@
                 <div class="card mt-1 color-component">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-2 col-md-2 col-lg-2 p-1">
+                            <div class="col-2 col-md-2 col-lg-3 p-1">
                             <p class="card-text">{{$bulletin->name}}</p>
                             </div>
-                            <div class="col-4 col-md-4 col-lg-4 p-1">
+                            <div class="col-4 col-md-5 col-lg-5 p-1">
                                 <p class="card-text">{{$bulletin->description}}</p>
                             </div>
-                            <div class="col-3 col-md-3 col-lg-1 p-1">
+                            <div class="col-3 col-md-3 col-lg-2 p-1">
                                 <p class="card-text">{{$bulletin->publication_date->format('j F Y')}}</p>
                             </div>
-                            <div class="col-3 col-md-3 col-lg-5 d-flex justify-content-end p-1">
+                            <div class="col-3 col-md-2 col-lg-2 d-flex justify-content-center p-1">
                                 <div class="card-text text-muted">
                                     <form method="POST" action="{{ route('publish.bulletin', $bulletin) }}">
                                         {{ csrf_field() }}
                                         @if ($bulletin->generatedInformations()->count() == 0)
-                                            <span class="badge badge-light"> 0 informaciones generadas.</span>
-                                            <span class="badge badge-primary">Este boletín no está publicado.</span>
+                                            <div class="d-flex justify-content-center mb-1">
+                                                <span class="badge badge-light"> 0 informaciones generadas.</span>
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                <span class="badge badge-primary">Este boletín no está publicado.</span>
+                                            </div>
                                         @elseif ($bulletin->generatedInformations()->count() > 0)
                                             @if ($bulletin->generatedInformations()->count() == 1)
-                                                <span class="badge badge-light"> 1 información generada.</span>
+                                                <div class="d-flex justify-content-center mb-1">
+                                                    <span class="badge badge-light"> 1 información generada.</span>
+                                                </div>
                                             @else
-                                                <span class="badge badge-light">{{$bulletin->generatedInformations()->count()}}  informaciones generadas.</span>
+                                                <div class="d-flex justify-content-center">
+                                                    <span class="badge badge-light">{{$bulletin->generatedInformations()->count()}}  informaciones generadas.</span>
+                                                </div>
                                             @endif
                                             @if($bulletin->is_published == false)
-                                                <span class="badge badge-primary">Este boletín no está publicado.</span>
-                                                <button type="submit" class="btn btn-light border border-dark">Publicar
-                                                </button>
+                                                <div class="d-flex justify-content-center mb-1">
+                                                    <span class="badge badge-primary">Este boletín no está publicado.</span>
+                                                </div>
+                                                <div class="d-flex justify-content-center">
+                                                    <button type="submit" class="btn btn-light border border-dark">Publicar</button>
+                                                </div>
                                             @else
-                                                <span class="badge badge-primary">Boletín publicado.</span>
+                                                <div class="d-flex justify-content-center">
+                                                    <span class="badge badge-primary">Boletín publicado.</span>
+                                                </div>
                                             @endif
                                         @endif
                                     </form>
